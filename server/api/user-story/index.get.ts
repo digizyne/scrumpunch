6fae672cc51161650b38530036f866cbef615d40
Joshua Castellucci <joshua.castellucci@digizyne.net>
2024-12-31 20:57:11 -0500

    Adding features to user-stories

--- server/api/user-story/index.get.ts
+++ server/api/user-story/index.get.ts
@@ -1,9 +1,28 @@
 import { pool } from "~/database";
+import { z } from "zod";
 
-export default defineEventHandler(async () => {
+const querySchema = z.object({
+    include_features: z.enum(["true", "false"]).optional(),
+});
+
+export default defineEventHandler(async (event) => {
     try {
         const res = await pool.query('SELECT * FROM user_story ORDER BY updated_at DESC');
-        return res.rows as UserStory[];
+        let { rows: userStories } = res as { rows: UserStory[] };
+
+        const { include_features } = await getValidatedQuery(event, querySchema.parse);
+        if (include_features !== undefined && include_features === "true") {
+            console.log("here")
+            userStories = await Promise.all(userStories.map(async (story) => {
+                const featureRes = await pool.query('SELECT * FROM feature WHERE user_story_id=$1', [story.id]);
+                const { rows: features } = featureRes;
+                return {
+                    ...story,
+                    features,
+                }
+            }));
+        }
+        return userStories;
     }
     catch (err) {
         console.log(err);
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,22 @@
 export {
-    UserStory
-};
+    UserStory,
+    Feature,
+}
 
 declare global {
     interface UserStory {
         id: string;
         description: string;
+        created_at?: Date;
+        updated_at?: Date;
+        archived_at?: Date;
+        features?: Feature[];
+    }
+
+    interface Feature {
+        id: string;
+        description: string;
+        user_story_id: string;
         created_at?: Date;
         updated_at?: Date;
         archived_at?: Date;
